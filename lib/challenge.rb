b9b8e6b737da92a339b71b12b13a8ac4b5d5ed8f
sjf125 <sjf125@gmail.com>
2016-05-24 09:28:36 -0400

    Get all but one test on challenge

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,3 +1,47 @@
 def calc(expr)
-  # Your code here
+  arr = expr.split(' ')
+  if arr.length == 0
+    puts 0
+    return 0
+  end
+  if arr[-1] == '+'
+    sum = 0
+    arr.pop
+    arr.each do |i|
+      sum += i.to_f
+    end
+    puts sum
+    return sum
+  end
+  if arr[-1] == '-'
+    diff = arr[0].to_f
+    arr.pop
+    arr.shift
+    arr.each do |i|
+      diff -= i.to_f
+    end
+    puts diff
+    return diff
+  end
+  if arr[-1] == '*'
+    prod = 1
+    arr.pop
+    arr.each do |i|
+      prod *= i.to_f
+    end
+    puts prod
+    return prod
+  end
+  if arr[-1] == '/'
+    div = arr[0].to_f
+    arr.pop
+    arr.shift
+    arr.each do |i|
+      div /= i.to_f
+    end
+    puts div
+    return div
+  end
 end
+
+calc('5 10 -')
